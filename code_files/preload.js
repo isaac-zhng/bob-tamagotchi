10b6dcce75e22c23cb77715edde6a3343f3c3a26
Lyria Zhu <lyriazhu@gmail.com>
2026-07-07 16:23:21 -0400

    Added fallback for happiness data

--- code_files/preload.js
+++ code_files/preload.js
@@ -1,4 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
+
 contextBridge.exposeInMainWorld('electronAPI', {
   openBobPopup:   () => ipcRenderer.send('open-bob-popup'),
   closeBobPopup:  () => ipcRenderer.send('close-bob-popup'),
@@ -9,5 +10,5 @@ contextBridge.exposeInMainWorld('electronAPI', {
   moveBobWindow:  (x, y) => ipcRenderer.send('bob-move', { x, y }),
   storeSave:      (stateJSON) => ipcRenderer.send('store-save', stateJSON),
   storeLoad:      () => ipcRenderer.invoke('store-load'),
-  isElectron: true,
+  isElectron:     true,
 });
